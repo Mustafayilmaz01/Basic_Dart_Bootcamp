d07ab5a8acedc22dac844ff28858bf7656404431
Mustafayilmaz01 <mustafayadana001@gmail.com>
2025-07-24 22:20:59 +0300

    darta basladim

---
+++ dart_dersleri/lib/degiskenler/degisken_olusturma.dart
@@ -0,0 +1,9 @@
+void main(){
+  int urunId= 3116;
+  String urunAdi = "Kol Saati";
+  int urunAdet = 100;
+  double urunFiyat = 149.99 ;
+  String urunTedarikci = "Rolex";
+
+  print("Urun Id: $urunId");
+}
---
+++ dart_dersleri/lib/giris/deneme.dart
@@ -0,0 +1,7 @@
+void main(){
+  String name = "Mustafa";
+  int yas =23;
+  double boy= 178;
+  var basHarf = "M";
+  print(name);
+}
